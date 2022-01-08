2e317ce1be51ad4f04b3d7d6ba0f67bd89d32ffa
timalo <tmalo@student.oulu.fi>
2022-01-08 17:11:48 +0200

    very minimal changes

---
+++ script.js
@@ -0,0 +1,13 @@
+const gameBoard = () => {
+    let board = [];
+}
+
+const player = (name) => {
+    let points = 0;
+    const getName = () => console.log(name);
+    return {getName};
+};
+
+const gameController = () => {
+
+}
